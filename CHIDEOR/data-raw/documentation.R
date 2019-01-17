4a53737ce58fcde40eac950c3f6a52c8b8250342
simazhi <thomas_van_hoey@hotmail.com>
2019-01-17 16:51:10 +0800

    updated version CHIDEOR

---
+++ CHIDEOR/data-raw/documentation.R
@@ -0,0 +1,43 @@
+#' CHIDEOR
+#' A data package for CHIDEOR.
+#' @docType package
+#' @aliases CHIDEOR-package
+#' @title Chinese Ideophone Database
+#' @name CHIDEOR
+#' @description A description of the data package
+#' @details Use \code{data(package='CHIDEOR')$results[, 3]} to see a list of available data sets in this data package
+#'     and/or DataPackageR::load_all
+#' _datasets() to load them.
+#' @seealso
+#' \link{full_database}
+NULL
+
+
+
+#' Detailed description of the data
+#' @name full_database
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{tbl_df} containing the following fields:
+#' \describe{
+#' \item{pinyinnum}{}
+#' \item{pinyintone}{}
+#' \item{pinyinnone}{}
+#' \item{traditional}{}
+#' \item{simplified}{}
+#' \item{MC}{}
+#' \item{OC}{}
+#' \item{radsup}{}
+#' \item{morph}{}
+#' \item{variant}{}
+#' \item{Kroll}{}
+#' \item{zdic}{}
+#' \item{note}{}
+#' }
+#' @source The data comes from________________________.
+#' @seealso
+#' \link{CHIDEOR}
+NULL
+
+
+
