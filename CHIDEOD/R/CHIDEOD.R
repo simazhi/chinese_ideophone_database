5186567077827f7e68dacf5725d615c9d7ff453b
simazhi <thomas_van_hoey@hotmail.com>
2019-10-07 12:58:06 +0800

    Chideod new trial

---
+++ CHIDEOD/R/CHIDEOD.R
@@ -0,0 +1,92 @@
+#' CHIDEOD
+#' A data package for CHIDEOD.
+#' @docType package
+#' @aliases CHIDEOD-package
+#' @title Package Title
+#' @name CHIDEOD
+#' @description A description of the data package
+#' @details Use \code{data(package='CHIDEOD')$results[, 3]} to see a list of available data sets in this data package
+#'     and/or DataPackageR::load_all
+#' _datasets() to load them.
+#' @seealso
+#' \link{chideod}
+NULL
+
+
+
+
+#' Detailed description of the data
+#' @name chideod
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{tbl_df} containing the following fields:
+#' \describe{
+#' \item{traditional}{}
+#' \item{simplified}{}
+#' \item{pinyintone}{}
+#' \item{pinyinnum}{}
+#' \item{pinyinnone}{}
+#' \item{HYDCD}{}
+#' \item{zdic}{}
+#' \item{Kroll}{}
+#' \item{MC}{}
+#' \item{OC}{}
+#' \item{radsup}{}
+#' \item{S1}{}
+#' \item{S2}{}
+#' \item{S3}{}
+#' \item{S4}{}
+#' \item{T1}{}
+#' \item{T2}{}
+#' \item{T3}{}
+#' \item{T4}{}
+#' \item{morphology}{}
+#' \item{S1.charfreq}{}
+#' \item{S2.charfreq}{}
+#' \item{S3.charfreq}{}
+#' \item{S4.charfreq}{}
+#' \item{S1.famfreq}{}
+#' \item{S2.famfreq}{}
+#' \item{S3.famfreq}{}
+#' \item{S4.famfreq}{}
+#' \item{S1.sem}{}
+#' \item{S2.sem}{}
+#' \item{S3.sem}{}
+#' \item{S4.sem}{}
+#' \item{S1.semfreq}{}
+#' \item{S2.semfreq}{}
+#' \item{S3.semfreq}{}
+#' \item{S4.semfreq}{}
+#' \item{S1.semfam}{}
+#' \item{S2.semfam}{}
+#' \item{S3.semfam}{}
+#' \item{S4.semfam}{}
+#' \item{S1.phon}{}
+#' \item{S2.phon}{}
+#' \item{S3.phon}{}
+#' \item{S4.phon}{}
+#' \item{S1.phonfreq}{}
+#' \item{S2.phonfreq}{}
+#' \item{S3.phonfreq}{}
+#' \item{S4.phonfreq}{}
+#' \item{S1.phonfam}{}
+#' \item{S2.phonfam}{}
+#' \item{S3.phonfam}{}
+#' \item{S4.phonfam}{}
+#' \item{sensorymod}{}
+#' \item{variant}{}
+#' \item{note}{}
+#' \item{datasource}{}
+#' \item{stage}{}
+#' \item{ASBC.dp}{}
+#' \item{ASBC.philosophy}{}
+#' \item{ASBC.literature}{}
+#' \item{ASBC.life}{}
+#' \item{ASBC.society}{}
+#' \item{ASBC.science}{}
+#' \item{ASBC.art}{}
+#' }
+#' @source The data comes from________________________.
+#' @seealso
+#' \link{CHIDEOD}
+NULL
